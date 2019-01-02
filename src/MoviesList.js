ba4b2e87f8366cdf358b8de1b983961ad7975c29
alvin.delossantos <alvin.delossantos@rstor.io>
2019-01-02 10:44:20 -0800

    Get API working

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -9,9 +9,11 @@ class MoviesList extends PureComponent {
   };
 
   async componentDidMount() {
+    const API_KEY = process.env.REACT_APP_API_KEY;
+
     try {
       const res = await fetch(
-        'https://api.themoviedb.org/3/discover/movie?api_key=hi&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`,
       );
       const movies = await res.json();
       this.setState({
